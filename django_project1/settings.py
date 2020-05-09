167dac92acd08453367300adcc83bcb662cd59fa
Teodorico S. Mazivila <teodoricom@hotmail.com>
2020-05-09 08:59:26 +0200

    Correcting config.py
    I have updated the config file to a much secure version

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jj+@_ss+auw$@6h9w_0@x_=qm3qmglro%%d+u=9l82lc3*k=8t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'teosweb77@gmail.com'  # os.environ.get('EMAIL_USER2')
-EMAIL_HOST_PASSWORD = 'ywvqdfvsgcnaietx'  # os.environ.get('EMAIL_PASS2')
+EMAIL_HOST_USER =  os.environ.get('EMAIL_USER2')
+EMAIL_HOST_PASSWORD =   os.environ.get('EMAIL_PASS2')
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 #EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
